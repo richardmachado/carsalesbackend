9cc3f2f0bf82ceae29dfac1914153b8328cd894b
Richard Machado <mmachado.richard@gmail.com>
2020-12-10 18:08:38 -0800

    that didn't work at all

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ server.get('/', (req, res) => { res.send("Server is running"); });
 
 server.get('/api/images', async (req, res) => {
     const { resources } = await cloudinary.search
-        .expression('folder:new_folder')
+        .expression('folder:dev_setups')
         .sort_by('public_id', "desc")
         .execute();
     const publicIds = resources.map(file => file.public_id);
@@ -36,7 +36,7 @@ server.post('/api/upload', async(req,res) => {
     try {
         const fileStr = req.body.data;
         const uploadedResponse = await cloudinary.uploader.upload(fileStr, {
-            upload_preset: 'new_images'
+            upload_preset: 'dev_setups'
         })
         console.log(uploadedResponse);
         res.json({msg:"you did it!"})
